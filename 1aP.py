b1dca7752435b731a59ed76cdfd74f719cee38db
Abid Asif <amasif@my.waketech.edu>
2025-02-06 13:41:36 -0500

    Erros commited

--- 1aP.py
+++ 1aP.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a number: "))
 def check_even_odd(num):
-    if num % 2 == 0:
+    if num - 2 == 0:
         return "Even"
     else:
         return "Odd"
--- 2cP.py
+++ 2cP.py
@@ -1,7 +1,7 @@
 number = int(input("Enter a number: "))
 def factorial(n):
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, n + 2):
         result *= i
     return result
 
