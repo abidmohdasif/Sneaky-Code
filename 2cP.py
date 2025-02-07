8941a0b850fe391b80f4ad1687269237d597c4e8
Abid Asif <amasif@my.waketech.edu>
2025-02-07 13:48:52 -0500

    Done

--- 2cP.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Enter a number: "))
-def factorial(n):
-    result = 1
-    for i in range(1, n + 2):
-        result *= i
-    return result
-
-print(factorial(number))
---
+++ Fixed Code/1aP.py
@@ -0,0 +1,27 @@
+num = int(input("Enter a number: "))
+def check_even_odd(num):
+    if num - 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(check_even_odd(num))
+#ORIGINAL
+#num = int(input("Enter a number: "))
+#def check_even_odd(num):
+#    if num - 2 == 0:
+#        return "Even"
+#    else:
+#        return "Odd"
+#
+#print(check_even_odd(num))
+
+#FIXED
+# num = int(input("Enter a number: "))
+# def check_even_odd(num):
+#     if num - 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# print(check_even_odd(num))
